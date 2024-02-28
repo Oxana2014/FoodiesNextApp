288fd81a7cec9b0ea396f0bdaaafca68628a51af
Oksana Kormilina <keysb@mail.ru>
2024-02-28 14:30:04 +0400

    Add structure for meals output

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,27 @@
-import Link from 'next/link'
+import Link from "next/link";
+
+import MealsGrid from "@/components/meals/meals-grid";
+
+import classes from "./page.module.css";
 
 export default function MealsPage() {
-    return <>
-    <h1>Meals</h1>
-    <p><Link href='/meals/share'>Share</Link></p>
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your favourite recipy</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
+  );
 }
