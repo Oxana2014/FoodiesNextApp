f4d8f9f5c8efeb61d529c845c54282834377c140
Oksana Kormilina <keysb@mail.ru>
2024-03-14 17:24:34 +0400

    Add server side validation of user input

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error({error}) {
+    //don't output becoming from next server error on the screen
+
+    return <main className="error">
+<h1>A Error occured!</h1>
+<p>Failed to create meal</p>
+    </main>
+}
